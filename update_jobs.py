7f2759000a9d061cc62b058efd5261306202c31f
Nimish Shinde <106652972+whynimoisalive@users.noreply.github.com>
2024-07-20 16:43:00 +0530

    fd

--- update_jobs.py
+++ update_jobs.py
@@ -2,7 +2,6 @@ import requests
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 import os
-import re
 
 # URL of the Google Spreadsheet exported as an Excel file
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1MvcSBIFc6hgd2bqN1NEEhfgcxp9NfPwT9qicIVFiwXA/export?format=xlsx'
@@ -15,22 +14,6 @@ with open('jobs.xlsx', 'wb') as file:
 # Load the data into a DataFrame
 df = pd.read_excel('jobs.xlsx', sheet_name='Client_Job_Posts')
 
-# Remove rows where 'Post' is NaN
-df = df.dropna(subset=['Post'])
-
-# Function to remove text within brackets
-def remove_brackets(text):
-    if isinstance(text, str):
-        return re.sub(r'\[.*?\]|\(.*?\)', '', text).strip()
-    return text
-
-# Clean data by removing text within brackets
-df['Post'] = df['Post'].apply(remove_brackets)
-df['Client Name'] = df['Client Name'].apply(remove_brackets)
-df['Job Location'] = df['Job Location'].apply(remove_brackets)
-df['Qualification'] = df['Qualification'].apply(remove_brackets)
-df['Responsibility'] = df['Responsibility'].apply(remove_brackets)
-
 # Setup Jinja2 environment
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('template.html')
@@ -42,6 +25,8 @@ html_output = template.render(jobs=df.to_dict(orient='records'))
 if os.path.exists('index.html'):
     os.remove('index.html')
 
-# Save the rendered HTML to index.html
-with open('index.html', 'w') as file:
+# Save the rendered HTML to index.html with UTF-8 encoding
+with open('index.html', 'w', encoding='utf-8') as file:
     file.write(html_output)
+
+print("HTML file updated successfully.")
