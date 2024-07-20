fc979e7f5c766d0914013fa56246cfb07635c61c
Nimish Shinde <106652972+whynimoisalive@users.noreply.github.com>
2024-07-20 17:25:19 +0530

    e

--- update_jobs.py
+++ update_jobs.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
+import re
 
 # URL of the Google Spreadsheet exported as an Excel file
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1MvcSBIFc6hgd2bqN1NEEhfgcxp9NfPwT9qicIVFiwXA/export?format=xlsx'
@@ -13,6 +14,17 @@ with open('jobs.xlsx', 'wb') as file:
 # Load the data into a DataFrame
 df = pd.read_excel('jobs.xlsx', sheet_name='Client_Job_Posts')
 
+# Remove rows where 'Post' is NaN
+df = df.dropna(subset=['Post'])
+
+# Function to remove parentheses and their content
+def remove_parentheses(text):
+    return re.sub(r'\([^)]*\)', '', str(text)).strip()
+
+# Apply the function to all columns
+for column in df.columns:
+    df[column] = df[column].apply(remove_parentheses)
+
 # Setup Jinja2 environment
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('template.html')
@@ -20,6 +32,6 @@ template = env.get_template('template.html')
 # Render the template with job data
 html_output = template.render(jobs=df.to_dict(orient='records'))
 
-# Save the rendered HTML to index.html
-with open('index.html', 'w') as file:
+# Save the rendered HTML to index.html using UTF-8 encoding
+with open('index.html', 'w', encoding='utf-8') as file:
     file.write(html_output)
